5fe81d3e99c098a8e02780aa231a59dd600d3678
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2021-10-09 15:18:05 +0300

    add static asset hosting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const initHandlebars = require('./config/handlebars.js');
 
 const app = express();
 
 initHandlebars(app);
+
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.all('/', (req, res) => {
     res.render('index');
